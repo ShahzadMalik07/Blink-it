04a750cfa6701ba989fd1dd872d96642eebe8ba3
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-01-07 23:52:33 +0530

    assets

--- frontend/src/components/Headers.jsx
+++ frontend/src/components/Headers.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import logo from "../assets/logo.png"
 
 const Headers = () => {
   return (
-    <header className='h-24 shadow-md p-2 sticky top-0 '>
-      <div>Logo</div>
+    <header className='h-24  shadow-md p-2 sticky top-0 '>
+      <div className='h-full '>
+        <div className='h-full container mx-auto flex items-center '>
+          <div>
+            <img src={logo} width={120} height={70} alt="logo" />
+          </div>
+          <div>Search</div>
+          <div>Login and my cart</div>
+
+
+        </div>
+      </div>
     </header>
   )
 }
