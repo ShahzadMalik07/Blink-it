b930f123a7b90f9d4388d47844dfa358879d5d24
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-01-19 23:42:28 +0530

    accessToken sent to headers from localstorage

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -7,4 +7,20 @@ const Axios = axios.create({
 
 })
 
+Axios.interceptors.request.use(
+    async(config)=>{
+        const accessToken = localStorage.getItem("accessToken")
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`     
+            
+        }
+        return config
+
+},(error)=>{
+    return Promise.reject(error)
+}
+)
+
+
+
 export default Axios
