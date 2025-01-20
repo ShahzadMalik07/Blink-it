031b5dfe2c9d37bdd543b54908a470bb568b057d
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-01-20 23:47:00 +0530

    refreshtoken

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -16,11 +16,34 @@ Axios.interceptors.request.use(
         }
         return config
 
-},(error)=>{
+},
+(error)=>{
     return Promise.reject(error)
 }
 )
 
+Axios.interceptors.request.use(
+    (response)=>{
+        return response
+    },
+    async(error)=>{
+        let request = error.config
+        if (error.response.status === 401 && !request.retry) {
+            request.retry = true
+            const refreshToken = localStorage.getItem("refreshToken")
+            
+        }
+    }
+)
+
+const refreshAccessToken = async (refreshToken)=>{
+    try {
+        const response = await Axios({})
+    } catch (error) {
+        
+    }
+
+}
 
 
 export default Axios
