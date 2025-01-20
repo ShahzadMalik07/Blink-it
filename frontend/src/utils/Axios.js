e6fdff9004940190f33d043f16b195eabc8538aa
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-01-20 23:57:07 +0530

    refresh token updated

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -3,44 +3,61 @@ import { baseURL } from "../common/SummaryApi";
 
 const Axios = axios.create({
     baseURL: baseURL,
-    withCredentials:true
+    withCredentials: true
 
 })
 
 Axios.interceptors.request.use(
-    async(config)=>{
+    async (config) => {
         const accessToken = localStorage.getItem("accessToken")
         if (accessToken) {
-            config.headers.Authorization = `Bearer ${accessToken}`     
-            
+            config.headers.Authorization = `Bearer ${accessToken}`
+
         }
         return config
 
-},
-(error)=>{
-    return Promise.reject(error)
-}
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
 )
 
 Axios.interceptors.request.use(
-    (response)=>{
+    (response) => {
         return response
     },
-    async(error)=>{
+    async (error) => {
         let request = error.config
         if (error.response.status === 401 && !request.retry) {
             request.retry = true
             const refreshToken = localStorage.getItem("refreshToken")
-            
+            if (refreshToken) {
+                const newAccessToken = await refreshAccessToken(refreshToken)
+                if (newAccessToken) {
+
+                    request.headers.Authorization = `Bearer ${newAccessToken}`
+                    return Axios(request)
+                }
+            }
+
         }
+        return Promise.reject(error)
     }
 )
 
-const refreshAccessToken = async (refreshToken)=>{
+const refreshAccessToken = async (refreshToken) => {
     try {
-        const response = await Axios({})
+        const response = await axios.post("http://localhost:3000/api/user/refresh-token", {}, {
+            headers: {
+                Authorization: `Bearer ${refreshToken}`,
+            },
+        })
+        const accessToken = response.data.data.accessToken
+        localStorage.setItem("accessToken",accessToken)
+       return accessToken
     } catch (error) {
-        
+        console.log(error)
+
     }
 
 }
