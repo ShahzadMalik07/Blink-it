5debe63306d40cf6c5b5432013c24262d02f24d1
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-03-07 23:42:33 +0530

    subcategory ui code removed

--- frontend/src/pages/SubCategory.jsx
+++ frontend/src/pages/SubCategory.jsx
@@ -40,28 +40,6 @@ const SubCategory = () => {
         openSubCategory && <UploadSubCategory close={() => setopenSubcategory(false)} />
       }
 
-      <div className='p-3 grid grid-cols-2 lg:grid-cols-6 lg:ml-8'>
-        {
-          subCategoryData.map((cat) => {
-            return (
-              <div className='w-32 h-56 m-1 rounded shadow-md' key={cat._id}>
-                <img src={cat.image} alt={cat.name} className='w-full object-scale-down' />
-
-                <div className='flex gap-2  items-center'>
-                  <button onClick={() => {
-                    // seteditOpen(true)
-                    // seteditData(cat)
-                  }} className='flex-1 py-1 bg-green-100  text-green-600 hover:bg-green-200  rounded'>Edit</button>
-                  <button onClick={() => {
-                    // setdialogOpen(true)
-                    // setcategoryId(cat)
-                  }} className='flex-1 py-1 bg-red-100 text-red-500 hover:bg-red-200 rounded '>Delete</button>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
     </div>
   )
 }
