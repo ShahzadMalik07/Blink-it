94cc66dcf726aae513c1f71bd2c467886b824127
ShahzadMalik07 <shahzadmalik425@gmail.com>
2025-03-14 21:35:28 +0530

    commit

--- frontend/src/components/ImageView.jsx
+++ frontend/src/components/ImageView.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const ImageView = ({url}) => {
+const ImageView = ({ url }) => {
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 bg-neutral-900 bg-opacity-75'>
-      <div className='w-full max-w-md p-4'>
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-40 bg-neutral-900 bg-opacity-75'>
+      <div className='w-full max-w-md p-4 bg-white max-h-[80vh]'>
+        <button></button>
         <img src={url} alt="image" className='h-full w-full object-scale-down' />
       </div>
-      
+
     </div>
   )
 }
